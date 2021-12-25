2a5d2a04c7339a1a74ee32e02c908c0a1a66e42d
Kirill <kirill-nef@mail.ru>
2021-12-25 17:35:16 +0500

    Полностью пересобрана навигация

--- source/js/burger-menu.js
+++ source/js/burger-menu.js
@@ -1,13 +1,13 @@
-var navMain = document.querySelector(".main-nav__wrapper")
+var navMain = document.querySelector(".main-nav")
 var navToggle = document.querySelector(".main-nav__toggle")
 
 navToggle.addEventListener("click", function () {
-  if (navMain.classList.contains("main-nav__wrapper--hidden")) {
-    navMain.classList.remove("main-nav__wrapper--hidden")
+  if (navMain.classList.contains("main-nav__mobile-closed")) {
+    navMain.classList.remove("main-nav__mobile-closed")
     navToggle.classList.remove("main-nav__toggle--closed")
     navToggle.classList.add("main-nav__toggle--open")
   } else {
-    navMain.classList.add("main-nav__wrapper--hidden")
+    navMain.classList.add("main-nav__mobile-closed")
     navToggle.classList.remove("main-nav__toggle--open")
     navToggle.classList.add("main-nav__toggle--closed")
   }

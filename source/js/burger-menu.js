95c4a45efdedbc3cc617bf63cb9af11b533cab76
Kirill <kirill-nef@mail.ru>
2022-01-23 11:57:37 +0500

    Д16. Исправлено + другие мелкие ошибки

--- source/js/burger-menu.js
+++ source/js/burger-menu.js
@@ -1,14 +1,12 @@
 const navMain = document.querySelector(".main-nav")
 const navToggle = document.querySelector(".main-nav__toggle")
 const pageHeader = document.querySelector(".page-header")
-const mapFrame = document.querySelector(".contacts__map-frame")
 
 pageHeader.classList.remove("page-header--js")
 navToggle.classList.remove("main-nav__toggle--js")
 navToggle.classList.remove("main-nav__toggle--open")
 navToggle.classList.add("main-nav__toggle--closed")
 navMain.classList.add("main-nav__mobile--closed")
-mapFrame.classList.add("contacts__map-frame--js")
 
 navToggle.addEventListener("click", function () {
   if (navMain.classList.contains("main-nav__mobile--closed")) {
---
+++ source/js/map-on.js
@@ -0,0 +1,2 @@
+const mapFrame = document.querySelector(".contacts__map-frame")
+mapFrame.classList.add("contacts__map-frame--js")
